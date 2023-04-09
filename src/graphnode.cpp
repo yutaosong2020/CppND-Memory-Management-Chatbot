9566317d0905d12f10b8d987ddf37d12c9e05b10
Yutao Song <yutao.song@burohappold.com>
2023-04-09 18:30:37 +0100

    task00_warmup_debug

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    // delete _chatBot; // no new pointer in the constructor 
 
     ////
     //// EOF STUDENT CODE
